bcb19fe464b9936dcabc9b4d777e8e5db9b68885
Deckason <chibuikemchidera@gmail.com>
2024-04-23 13:11:40 +0100

    Created the splash screen

---
+++ src/app/components/splashScreens/SplashScreen1.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styles from "./splashScreen.module.css"
+import splashImg1 from "../../../../public/media/splash1.png"
+import Image from 'next/image'
+import Link from 'next/link'
+import { MdOutlineArrowForwardIos } from "react-icons/md";
+
+const SplashScreen1 = ({content, SplashScreenCount, setSplashScreenCount}) => {
+  return (
+    <div className={`${styles.container}`}>
+        <Link className={`${styles.skip_button}`} href={"#"}>Skip</Link>
+        <div className={`${styles.splash_img}`}>
+            <Image 
+                src={content.img}
+                height={500}
+                width={700}
+                alt={"Splash-screen-img1"}
+            />
+        </div>
+        <div className={`${styles.splash_text_container}`}>
+            <h3 className={`${styles.splash_text_h3}`}>{content.header}</h3>
+            <p className={`${styles.splash_text_content}`}>{content.content}</p>
+        </div>
+        <div className={`${styles.next_splash_container}`}
+            onClick={()=>{console.log(SplashScreenCount); setSplashScreenCount(1)}}
+        >
+            <div className={`${styles.arc}`}>
+                <span><MdOutlineArrowForwardIos style={{color: "#fff"}}/></span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SplashScreen1
---
+++ src/app/page.js
@@ -0,0 +1,31 @@
+"use client"
+import React, { useState } from 'react'
+import SplashScreen1 from './components/splashScreens/SplashScreen1'
+import splashImg1 from "../../public/media/splash1.png"
+import splashImg2 from "../../public/media/splash2.png"
+
+const SplashScreenContent = [
+  {
+    img: splashImg1, 
+    header: "Track Your Goal", 
+    content: "Don’t worry if you have trouble determining your goals, We can help you determine your goals and track your goals"
+  },
+  {
+    img: splashImg2, 
+    header: "Get Burn", 
+    content: "Let’s keep burning to achieve your goals, it hurts only temporarily, if you give up now you will be in pain forever"
+  }
+]
+
+const page = () => {
+  const [SplashScreenCount , setSplashScreenCount  ] = useState(0)
+  return (<div className='main'>
+    <SplashScreen1 
+      content={SplashScreenContent[SplashScreenCount]} 
+      SplashScreenCount={SplashScreenCount} 
+      setSplashScreenCount={setSplashScreenCount}
+    />
+  </div>)
+}
+
+export default page
