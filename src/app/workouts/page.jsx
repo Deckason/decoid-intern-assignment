d04eb72be23627cf26117370f6dbe29cb47a0cfd
Deckason <chibuikemchidera@gmail.com>
2024-04-25 23:05:23 +0100

    Finished the last page

--- src/app/workouts/page.jsx
+++ src/app/workouts/page.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from "./page.module.css"
 import { RiArrowDropLeftLine } from "react-icons/ri";
 import ButtomNav from '../components/buttomNav/ButtomNav';
+import WorkoutSchedule from "../components/workoutComp/workoutSchedule/WorkoutSchedule"
+import AddWorkout from '../components/workoutComp/addWorkout/AddWorkout';
 
 const page = () => {
   return (
@@ -11,8 +13,9 @@ const page = () => {
         <h1>Workout Schedule</h1>
     </div>
     <div className={styles.main}>
-
+      <WorkoutSchedule />
     </div>
+    <AddWorkout />
     <ButtomNav />
 </div>
   )
